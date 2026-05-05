06bb9c6477fff43c81d08a6e48b2cfb33b05d146
sindhwalrishabh <76862401+sindhwalrishabh@users.noreply.github.com>
2026-05-05 19:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.35%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    double power(double x, long long n){
+        if( n == 0) return 1.0;
+
+        if(n == 1) return x;
+
+        if(n % 2 == 0){
+            return power(x*x , n/2);
+        }
+        return x * power(x , n-1);
+    }
+public:
+    double myPow(double x, long long n) {
+     long long num = n;
+
+        if(num < 0){
+            x = 1 / x;
+            num = -num;
+        }
+
+       return power(x,num);
+    }
+};
